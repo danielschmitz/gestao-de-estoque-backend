44832e13bbd959185988a7e5f647d7ce062e3ebd
Daniel Schmitz <danieljfa@gmail.com>
2020-08-10 09:11:37 -0300

    Adicionar Usuário

--- src/api/usuarios.js
+++ src/api/usuarios.js
@@ -1,8 +1,27 @@
 const db = require('../libs/knex')
+const bcrypt = require('bcryptjs')
+
+const tabela = 'usuarios'
 
 module.exports = app => {
     app.get('/usuarios', async function(req,res) {
-        const usuarios = await db('usuarios')
+        const usuarios = await db(tabela)
         return res.json(usuarios)
+    })
+    app.post('/usuario', async function(req,res, next) {
+        const { nome, email, senha } = req.body
+        
+        const emailExiste = await db(tabela).where({email}).first()
+        if (emailExiste) { return res.badRequest('Email Inválido!!') }
+        
+        const result = await db(tabela).insert(
+            {   
+                nome,
+                email,
+                senha: bcrypt.hashSync(senha, 10)
+            }
+        )
+        const id = result[0]
+        return res.json({id})
     })
 }
